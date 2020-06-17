095f5b3a292b7dc15cc9626fe18887d0bc76fdcc
Raymond Lai Jin Hao <rlaijinh@redhat.com>
2020-06-17 13:03:30 +0800

    added sample flask app .py file

--- main.tf
+++ main.tf
@@ -1,29 +1,59 @@
 provider "google" {
+  credentials = file("../optical-pillar-279806-8a8df30de4a6.json")
   project = "optical-pillar-279806"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
 
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+resource "google_compute_instance" "default" {
+  name         = "flask-vm"
   machine_type = "f1-micro"
+  zone         = "us-west1-a"
 
+metadata = {
+   ssh-keys = "raymond:${file("~/.ssh/id_rsa.pub")}"
+}
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
     }
   }
-
+ 
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask; python /home/raymond/app.py;}"
+  
   network_interface {
     # A default network is created for all GCP projects
-    network       = google_compute_network.vpc_network.self_link
+    network       = "default"
     access_config {
     }
   }
 }
 
+data "template_file" "default" {
+  template = file("app.py")
+}
+
+resource "local_file" "app" {
+  content = data.template_file.default.rendered
+  filename = "/home/raymond/app.py"
+}
+
 resource "google_compute_network" "vpc_network" {
   name                    = "terraform-network"
   auto_create_subnetworks = "true"
+}
+
+resource "google_compute_firewall" "default" {
+ name    = "flask-app-firewall"
+ network = "default"
+
+ allow {
+   protocol = "tcp"
+   ports    = ["5000"]
+ }
+}
+
+output "ip" {
+ value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
 }
